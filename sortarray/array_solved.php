890145ba83a216f69f48245cc03ff66ce489fbc2
TropicalToucan <56321738+TropicalToucan@users.noreply.github.com>
2021-11-17 23:24:38 -0500

    Add files via upload

---
+++ sortarray/array_solved.php
@@ -0,0 +1,25 @@
+﻿<?php
+    // create an array
+    $new_games = array();
+
+    $games = array_push($new_games,
+        "Farcry 4",
+        "Metal Gear",
+        "Fallout 4",
+        "Witcher 3",
+        "Batman",
+      "Call of duty",
+    "Battlefield");
+
+    // count number of items
+    $total_items = count($new_games);
+
+    // Sort the array
+    sort($new_games);
+
+    // Randomly select an item
+    $selected = rand(0, $total_items);
+
+    // echo
+    echo ($new_games[$selected]);
+?>
